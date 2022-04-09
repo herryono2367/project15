89abf3608b03ed7518372708089aa296d9629d20
herryono <herryono2367@gmail.com>
2022-04-09 16:33:12 +0800

    belajar route

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,31 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//routing default
 Route::get('/', function () {
     return view('upload');
+});
+Route::get('/mahasiswa', function(){
+    echo 'mahasiswa';
+});
+//routing dengan parameter
+Route::get('/ubg/ilkom/mahasiswa/{nim}', function($nim){
+    echo 'mahasiswa ilkom kampus ubg' .$nim;
+});
+
+Route::redirect('/login','/masuk');
+
+//groub get
+Route::group(['prefix'=>'/admin'], function(){
+
+
+Route::get('/mahasiswa', function(){
+    return 'Admin mahasiswa';
+});
+Route::get('/dosen', function(){
+    return 'Admin dosen';
+});
+Route::get('/pegawai', function(){
+    return 'Admin pegawai';
+});
 });

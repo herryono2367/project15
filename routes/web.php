aac3b0726725be31dcd2ba1bf16e816c38f394d0
herryono <herryono2367@gmail.com>
2022-04-11 13:36:23 +0800

    belajar root

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+Route::get('/', function () {
+    return view('upload');
+});
 
+
+Route::get('/mahasiswa', function(){
+    echo 'mahasiswa';
+});
+//routing dengan parameter
+Route::get('/ubg/ilkom/mahasiswa/{nim}', function($nim){
+    echo 'mahasiswa ilkom kampus ubg' .$nim;
+});
+Route::redirect('/login','/masuk');
+//groub get
+Route::group(['prefix'=>'/admin'], function(){
+Route::get('/mahasiswa', function(){
+    return 'Admin mahasiswa';
+});
+Route::get('/dosen', function(){
+    return 'Admin dosen';
+});
+Route::get('/pegawai', function(){
+    return 'Admin pegawai';
+});
+});
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,30 +37,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 //routing default
+
 Route::get('/', function () {
-    return view('upload');
-});
-Route::get('/mahasiswa', function(){
-    echo 'mahasiswa';
-});
-//routing dengan parameter
-Route::get('/ubg/ilkom/mahasiswa/{nim}', function($nim){
-    echo 'mahasiswa ilkom kampus ubg' .$nim;
+    return view('welcome');
 });
 
-Route::redirect('/login','/masuk');
+Route::get('/home', function(){
+return view('index');
+});
 
-//groub get
-Route::group(['prefix'=>'/admin'], function(){
-
-
-Route::get('/mahasiswa', function(){
-    return 'Admin mahasiswa';
-});
-Route::get('/dosen', function(){
-    return 'Admin dosen';
-});
-Route::get('/pegawai', function(){
-    return 'Admin pegawai';
-});
-});
